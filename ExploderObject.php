65b50dc395bf575c8b1e58c6db77c3da27cf970b
David J. Arias López M <david@ares>
2020-05-16 21:12:28 -0500

    Clase ExploderObject

---
+++ ExploderObject.php
@@ -0,0 +1,16 @@
+<?php
+
+class ExploderObject {
+	private $id;
+
+	public function __construct() {
+		$tmp = spl_object_hash($this);
+		$this->id = preg_replace('/^0+/','',substr($tmp,16,16)).preg_replace('/^0+/','',substr($tmp,0,16));
+	}
+
+	protected function getId() {
+		return $this->id;
+	}
+}
+
+?>
